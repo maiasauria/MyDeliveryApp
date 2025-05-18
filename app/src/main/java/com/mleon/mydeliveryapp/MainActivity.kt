a3d805e4e14a07ded66529ed18573287a5a7db1a
Mai <maiasauria1@gmail.com>
2025-05-18 12:28:39 -0300

    Add two interchangeable fragments and a counter.

--- app/src/main/java/com/mleon/mydeliveryapp/MainActivity.kt
+++ app/src/main/java/com/mleon/mydeliveryapp/MainActivity.kt
@@ -1,47 +1,24 @@
 package com.mleon.mydeliveryapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mleon.mydeliveryapp.ui.theme.MyDeliveryAppTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            MyDeliveryAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        val btnNavigate = findViewById<Button>(R.id.btnNavigateToDemo)
+
+        btnNavigate.setOnClickListener {
+            val intent = Intent(this, LifecycleDemoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyDeliveryAppTheme {
-        Greeting("Android")
-    }
-}
