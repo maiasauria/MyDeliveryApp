15a70c95938345e3aef4024fabffe65ad4237886
Mai <maiasauria1@gmail.com>
2025-06-06 16:09:52 -0300

    Cleanup unused Activities.

--- app/src/main/java/com/mleon/mydeliveryapp/view/ui/views/LoginScreen.kt
+++ app/src/main/java/com/mleon/mydeliveryapp/view/ui/views/LoginScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.mleon.mydeliveryapp.view.viewmodel.LoginViewModel
 
